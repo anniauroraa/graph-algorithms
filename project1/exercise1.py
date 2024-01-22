e1253e3819571157b3babd724b763c59219ff245
Anni <anni.a.virtanen@tuni.fi>
2024-01-22 18:55:39 +0200

    set up project environment

---
+++ project1/exercise1.py
@@ -0,0 +1,51 @@
+
+### Read in a graph, a set of vertices, and a pair of vertices. 
+
+import graph
+import sys
+
+## Implement your algorithm here:
+def algorithm(g, B, v, w):
+
+  print("adj:", g.adj)
+  print("w: ", g.w)
+  print("B:", B)
+  print("start & end:", v, w)
+  return 0
+
+
+### Read in a set of vertices from a file. These are just numbers separated by whitespace.
+def readset(filename):
+  f = open(filename, 'r')
+  s = set()
+  for line in f:
+    for v in line.split():
+      s.add(int(v))
+  return s
+
+## Read the pair, again, just two integers separated by whitespace.
+def readpair(filename):
+  f = open(filename, 'r')
+  for line in f:
+    (v,w) = line.split()
+    return (int(v), int(w))
+
+
+### If ran from the command line:
+if __name__ == "__main__":
+  # Graph is the first command line argument:
+  g = graph.Graph()
+  g.readgraph(sys.argv[1])
+  # Vertices are the second command line argument:
+  B = readset(sys.argv[2])
+  # Pair is the third command line argument:
+  (v,w) = readpair(sys.argv[3])
+
+  ### Call your algorithm:
+
+  n = algorithm(g, B, v, w)
+
+  # Print the result:
+  print(n)
+  
+
