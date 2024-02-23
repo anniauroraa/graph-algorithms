8f302279130668f77bb92979171a128ff93dad03
Anni <anni.a.virtanen@tuni.fi>
2024-02-23 12:21:14 +0200

    include source vertex to centrality calculations

--- project3/game_prediction.py
+++ project3/game_prediction.py
@@ -1,7 +1,8 @@
 from graph_fixed import Graph
-from copy import deepcopy as copy
 from queue import Queue
 import sys
+
+# INSTRUCTIONS: run on terminal: "python game_prediction.py [testfile1.txt]"
 
 class PredictionNetwork:
     def __init__(self,G) -> None:
@@ -14,7 +15,6 @@ class PredictionNetwork:
 
         # define the first vertex that has edges as the starting point of the graph (usually 1 or 0)
         for u in range(self.G.n):
-            # print(len(self.G.adj[u]))
             if len(self.G.adj[u]) > 0:
                 self.s = u
                 return
@@ -30,18 +30,14 @@ class PredictionNetwork:
 
         # go through the algorithm n times so that every vertex is ones the start vertex
         for start in range(self.s, self.G.n):
-            for vertice in range(self.G.n):
+            for vertice in range(self.s,self.G.n):
                 self.delta[vertice] = 0    
                 self.d[vertice] = None
                 self.sigma[(start,vertice)] = 0
                 self.pred[vertice] = []
             self.stack = []
             self.BrandesBFS(start)
-            #print(f"sigma {self.sigma}")
             self.MagicDelta(start)
-
-            # print(f"centralities {start}. {self.centralities}")
-
         return
     
     def BrandesBFS(self,s):
@@ -56,39 +52,33 @@ class PredictionNetwork:
         while not queue.empty():    
             current = queue.get()
             self.stack.append(current)
-            #print(f"stack iteration {self.stack}")
             for neighbor in self.G.adj[current]:
-                #print(neighbor)
                 if d[neighbor] == None:
                     d[neighbor] = d[current] + 1
                     queue.put(neighbor)
-
-                # print(d[neighbor], d[current])
                 if d[neighbor] == d[current] + 1:
                     self.sigma[(s,neighbor)] = self.sigma[(s,neighbor)] + self.sigma[(s,current)]
                     self.pred[neighbor].append(current)
-        # print(f"distances {self.d}")
         return
     
+
     def MagicDelta(self,s):
-        # print(f"stack {self.stack}")
         stack = self.stack
         sigma = self.sigma
         delta = self.delta 
 
         while len(stack) > 0:
             succ = stack.pop()
-            # print(f"going through stack {succ}")
             for pred in self.pred[succ]:
                 # delta magic equation
-                delta[pred] = delta[pred] + sigma[(s,pred)]/sigma[(s,succ)]*(1+delta[succ])
-            if succ != s:
-                self.centralities[succ] = self.centralities[succ] + delta[succ]
-
+                delta[pred] += (sigma[(s,pred)]/sigma[(s,succ)])*(1+delta[succ])
+            # print(f"centrality for {succ}: {self.centralities[succ]} + {delta[succ]}\n")
+            self.centralities[succ] += delta[succ]
         return
     
-    def topTen(self, C):
-        sorted_players = sorted(C.items(), key=lambda x: x[1], reverse=True)
+
+    def topTen(self):
+        sorted_players = sorted(self.centralities.items(), key=lambda x: x[1], reverse=True)
         top_10 = [player[0] for player in sorted_players[:10]]
         return top_10
 
@@ -99,8 +89,8 @@ if __name__ == "__main__":
     G.readgraph(inputgraph)
     F = PredictionNetwork(G)
     F.BFSHighLevel()
-    winners = F.topTen(F.centralities)
+    winners = F.topTen()
 
-    print(f"centralities {F.centralities}")
-    print("------------------")
-    print("The top 10 players: " + str(winners))
+    # print(f"centralities {F.centralities}")
+    # print("------------------")
+    print(f"The top 10 players: {winners}")
